74844d264600b33addcba0680f99d85198e6fd2f
Rui Vieira <ruidevieira@googlemail.com>
2021-01-31 22:34:03 +0000

    Add real estate model

--- real-estate-price/src/data/make_dataset.py
+++ real-estate-price/src/data/make_dataset.py
@@ -3,21 +3,42 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
+import pandas as pd
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
+
+    input_file = os.path.join(input_filepath, "data.zip")
+    logger.info(f"importing zipped data {input_file}")
+    df = pd.read_csv(input_file)
+    df = df[
+        ["CRIM", "ZN", "INDUS", "NOX", "RM", "AGE", "DIS", "RAD", "PTRATIO", "MEDV"]
+    ]
+
+    df = df.dropna()
+    inputs = df[["CRIM", "ZN", "INDUS", "NOX", "RM", "AGE", "DIS", "RAD", "PTRATIO"]]
+
+    outputs = df[["MEDV"]]
+
+    inputs_dest_file = os.path.join(output_filepath, "inputs.csv")
+    outputs_dest_file = os.path.join(output_filepath, "outputs.csv")
+
+    logger.info("exporting inputs.csv and outputs.csv")
+    inputs.to_csv(inputs_dest_file, index=False)
+    outputs.to_csv(outputs_dest_file, index=False)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
