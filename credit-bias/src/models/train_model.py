43e532d65d030e5aefe13e21923db37b3ca7dfd3
Rui Vieira <ruidevieira@googlemail.com>
2022-12-20 11:44:41 +0000

    Add customisable XGBoost format for credit-bias

--- credit-bias/src/models/train_model.py
+++ credit-bias/src/models/train_model.py
@@ -27,16 +27,21 @@ logger = colorlog.getLogger(__name__)
 
 
 @click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath, output_filepath):
+@click.option("-i", "--input_filepath", type=click.Path(exists=True))
+@click.option("-o", "--output_filepath", type=click.Path())
+@click.option("-f", "--output_format")
+def main(input_filepath, output_filepath, output_format):
+    print("Options")
+    print(input_filepath)
+    print(output_format)
+    print(output_filepath)
     """Trains a model from processed data into a joblib-serialised model"""
 
     # load processed dataset
     logger.info("Loading processed dataset")
     _df: DataFrame = pd.read_csv(os.path.join(input_filepath, "train.csv"))
 
-    if _df:
+    if not _df.empty:
 
         X_df: Optional[DataFrame] = _df.drop("PaidLoan", axis=1)
         y_df: DataFrame = _df["PaidLoan"]
@@ -91,58 +96,64 @@ def main(input_filepath, output_filepath):
 
         xgb_model.fit(train_x, train_y)
 
-        joblib.dump(xgb_model, output_filepath)
-        logger.info("Saved model")
-
-        pipeline = PMMLPipeline([("classifier", xgb_model)])
-
-        logger.info("Saving PMML model")
-        skl_to_pmml(
-            pipeline,
-            [
-                "NewCreditCustomer",
-                "Amount",
-                "Interest",
-                "LoanDuration",
-                "Education",
-                "NrOfDependants",
-                "EmploymentDurationCurrentEmployer",
-                "IncomeFromPrincipalEmployer",
-                "IncomeFromPension",
-                "IncomeFromFamilyAllowance",
-                "IncomeFromSocialWelfare",
-                "IncomeFromLeavePay",
-                "IncomeFromChildSupport",
-                "IncomeOther",
-                "ExistingLiabilities",
-                "RefinanceLiabilities",
-                "DebtToIncome",
-                "FreeCash",
-                "CreditScoreEeMini",
-                "NoOfPreviousLoansBeforeLoan",
-                "AmountOfPreviousLoansBeforeLoan",
-                "PreviousRepaymentsBeforeLoan",
-                "PreviousEarlyRepaymentsBefoleLoan",
-                "PreviousEarlyRepaymentsCountBeforeLoan",
-                "Council_house",
-                "Homeless",
-                "Joint_ownership",
-                "Joint_tenant",
-                "Living_with_parents",
-                "Mortgage",
-                "Other",
-                "Owner",
-                "Owner_with_encumbrance",
-                "Tenant",
-                "Entrepreneur",
-                "Fully",
-                "Partially",
-                "Retiree",
-                "Self_employed",
-            ],
-            "PaidLoan",
-            output_filepath + ".pmml",
-        )
+        if output_format == "json":
+            xgb_model.save_model(output_filepath + ".json")
+            logger.info("Saving model as JSON")
+        elif output_format == "ubj":
+            logger.info("Saving model as Universal Binary JSON (UBJ)")
+            xgb_model.save_model(output_filepath + ".ubj")
+        elif output_format == "pmml":
+            logger.info("Saving model as PMML")
+            pipeline = PMMLPipeline([("classifier", xgb_model)])
+            logger.info("Saving PMML model")
+            skl_to_pmml(
+                pipeline,
+                [
+                    "NewCreditCustomer",
+                    "Amount",
+                    "Interest",
+                    "LoanDuration",
+                    "Education",
+                    "NrOfDependants",
+                    "EmploymentDurationCurrentEmployer",
+                    "IncomeFromPrincipalEmployer",
+                    "IncomeFromPension",
+                    "IncomeFromFamilyAllowance",
+                    "IncomeFromSocialWelfare",
+                    "IncomeFromLeavePay",
+                    "IncomeFromChildSupport",
+                    "IncomeOther",
+                    "ExistingLiabilities",
+                    "RefinanceLiabilities",
+                    "DebtToIncome",
+                    "FreeCash",
+                    "CreditScoreEeMini",
+                    "NoOfPreviousLoansBeforeLoan",
+                    "AmountOfPreviousLoansBeforeLoan",
+                    "PreviousRepaymentsBeforeLoan",
+                    "PreviousEarlyRepaymentsBefoleLoan",
+                    "PreviousEarlyRepaymentsCountBeforeLoan",
+                    "Council_house",
+                    "Homeless",
+                    "Joint_ownership",
+                    "Joint_tenant",
+                    "Living_with_parents",
+                    "Mortgage",
+                    "Other",
+                    "Owner",
+                    "Owner_with_encumbrance",
+                    "Tenant",
+                    "Entrepreneur",
+                    "Fully",
+                    "Partially",
+                    "Retiree",
+                    "Self_employed",
+                ],
+                "PaidLoan",
+                output_filepath + ".pmml",
+            )
+        else:
+            raise ValueError(f"Format {output_format} not supported.")
 
 
 if __name__ == "__main__":
